7910e006e5df9f19a804ace8cfb373261ed5cbc5
Sumit240803 <goyalsumit651@gmail.com>
2025-07-15 09:53:48 -0700

    Verifiable Credentials

--- src/stealth-dao-backend/src/lib.rs
+++ src/stealth-dao-backend/src/lib.rs
@@ -1,4 +1,27 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::collections::HashMap;
+
+use ic_verifiable_credentials::{build_credential_jwt, issuer_api::{ArgumentValue, CredentialSpec}, CredentialParams};
+
+#[ic_cdk::update]
+pub fn create_credential_jwt(
+    credential_type : String ,
+    credential_args :Option<HashMap<String, ArgumentValue>>,
+    
+    credential_id_url: String,
+    issuer_url: String,
+    expiration_timestamp_s: u32, 
+)->String{
+    let params = CredentialParams {
+        spec :CredentialSpec{
+            credential_type : credential_type,
+            arguments : credential_args
+        },
+        subject_id : format!("https://github.com/Sumit240803/stealth-dao-WCHL-2025/credentials/{}",ic_cdk::api::msg_caller().to_string()),
+        credential_id_url : credential_id_url.to_string(),
+        issuer_url : issuer_url.to_string(),
+        expiration_timestamp_s : expiration_timestamp_s
+
+    };
+
+    build_credential_jwt(params)
 }
